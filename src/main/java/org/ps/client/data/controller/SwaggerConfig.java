d7c22159ac91ada639870dcbd18c8f6f70df8a48
paulstroie <paul.stroie@hotmail.com>
2020-12-28 14:07:21 +0200

    Swagger title

--- src/main/java/org/ps/client/data/controller/SwaggerConfig.java
+++ src/main/java/org/ps/client/data/controller/SwaggerConfig.java
@@ -28,8 +28,8 @@ public class SwaggerConfig implements WebMvcConfigurer {
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Swagger interface")
-                .description("API Interface Test")
+                .title("Rest Client Service")
+                .description("API Interface")
                 .license("License Version 1.0")
                 .version("1.0").build();
     }
